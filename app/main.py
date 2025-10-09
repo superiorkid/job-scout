2184aa83ad9063b5a5be0f56c25f4f5b1973f33f
superiorkid <mohammad.ilhamuddin@gmail.com>
2025-10-09 23:58:42 +0800

    remove jinja2 package

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
-from fastapi import FastAPI, Request
-from starlette.responses import HTMLResponse
-from starlette.staticfiles import StaticFiles
-from starlette.templating import Jinja2Templates
-
+from fastapi import FastAPI
 app = FastAPI(description="Job Scout API")
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="app/templates")
-
-@app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html")
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
